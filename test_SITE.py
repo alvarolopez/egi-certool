54b851e5c3cccdbf3feaee7ed950d164e93d322e
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2011-08-11 10:46:58 +0200

    first working version

--- test_SITE.py
+++ test_SITE.py
@@ -28,7 +28,7 @@ def check_ces(bdii, vo):
     
     ces = ret[-1].splitlines()
     
-    print "\t\tFound: " + "\n\t\t\t".join(ces)
+    print "\t\tFound: " + ",\n\t\t\t".join(ces)
 
     checked = []
     for ce in ces:
@@ -64,7 +64,7 @@ def check_ces(bdii, vo):
                 url = ret[1].splitlines()[-1]
             else:
                 continue
-    
+            print "\t\t\tJob ID: %s" % url
             while True:
                 ret = commands.getstatusoutput("glite-ce-job-status -n %s" % url)
                 if "[DONE-OK]" in ret[1]:
@@ -78,6 +78,7 @@ def check_ces(bdii, vo):
             rets.append(ret)
     
         else:
+            # I will not waste much effort on this, since lcg-CE are condemned to disappear.
             print "\t\tchecking globus-job-run to ce"
             ret = commands.getstatusoutput("globus-job-run %s /bin/hostname" % aux) 
             print_error(ret)
@@ -100,19 +101,34 @@ def check_ces(bdii, vo):
             print "\t\tJob submission has problems, check above errors"
 
 
+def filter_and_join_ldap(data, query):
+    """Filter results to only those of query and join line breaks from ldapsearch."""
+    got = False
+    aux = []
+    for i in data.splitlines():
+        if i.startswith(query):
+            got = True
+            aux.append([i.split(":",1)[-1].strip()])
+        elif i.startswith(" ") and got:
+            aux[-1].append(i.strip())
+        elif got:
+            got = False
+    return ["".join(i) for i in aux]
+
+
 def check_ses(bdii, vo):
     print
     print "#" * 80
     print "Checking Storage Elements"
     print "\tQuerying the BDII for the SEs"
 
-    ret = commands.getstatusoutput("lcg-info --list-se --bdii %(bdii)s --sed --vo %(vo)s" % locals())
+    ret = commands.getstatusoutput("lcg-info --list-se --bdii %(bdii)s --sed --vo VO:%(vo)s" % locals())
     print_error(ret, exit=True)
 
     ses = ret[-1].splitlines()
-    print "\t\tFound: " + "\n\t\t\t".join(ses)
+    print "\t\tFound: " + ",\n\t\t\t".join(ses)
 
-    checked = []
+    checked = ["gridce05.ifca.es"]
     for se in ses:
         if se in checked:
             continue
@@ -128,17 +144,33 @@ def check_ses(bdii, vo):
             print "\t\tGridFTP is up"
         rets.append(ret)
 
-        randfile = ''.join([choice(string.letters + string.digits) for i in range(15)])
 
-        print "\t\tchecking copy and register"
-        ret = commands.getstatusoutput("lcg-cr -v --vo %(vo)s -d %(se)s -l lfn:/grid/%(vo)s/%(randfile)s file:/etc/issue" % locals()) 
+        ret = commands.getstatusoutput("ldapsearch -x -LLL -H ldap://%(bdii)s -b o=grid '(&(objectClass=GlueSATop) (GlueVOInfoAccessControlBaseRule=VO:%(vo)s) (GlueChunkKey=GlueSEUniqueID=%(se)s))' GlueVOInfoPath" % locals())
         print_error(ret)
         rets.append(ret)
-        if ret[0] == 0:
-            print "\t\tchecking removal of uploaded file"
-            ret = commands.getstatusoutput("lcg-del -v --vo %(vo)s lfn:/grid/%(vo)s/%(randfile)s -a" % locals())
-            print_error(ret)
-            rets.append(ret)
+
+        se_paths = filter_and_join_ldap(ret[1], "GlueVOInfoPath")
+
+        ret = commands.getstatusoutput("ldapsearch -x -LLL -H ldap://%(bdii)s -b o=grid '(&(objectClass=GlueSEControlProtocol) (GlueChunkKey=GlueSEUniqueID=%(se)s) (GlueSEControlProtocolType=SRM) (GlueSEControlProtocolVersion=2.2.0))' GlueSEControlProtocolEndpoint" % locals())
+        print_error(ret)
+        rets.append(ret)
+
+        endpoints = [i.replace("httpg","srm") for i in filter_and_join_ldap(ret[1], "GlueSEControlProtocolEndpoint")]
+
+#        ret = commands.getstatusoutput("lcg-cp -v --vo %(vo)s file:/etc/issue srm://%(se)s/%(se_path)s/%(randfile)s" % locals()) 
+        for endpoint in endpoints:
+            for se_path in se_paths:
+                print "\t\tUploading file to %(endpoint)s/%(se_path)s" % locals()
+                randfile = ''.join([choice(string.letters + string.digits) for i in range(15)])
+                ret = commands.getstatusoutput("lcg-cp -v -b --vo %(vo)s -D srmv2 file:/etc/issue %(endpoint)s/\?SFN=%(se_path)s/%(randfile)s" % locals())
+                print_error(ret)
+                rets.append(ret)
+
+                if ret[0] == 0:
+                    print "\t\tRemoving uploaded file"
+                    ret = commands.getstatusoutput("lcg-del -l -v -b --vo %(vo)s -D srmv2 %(endpoint)s/\?SFN=%(se_path)s/%(randfile)s" % locals())
+                    print_error(ret)
+                    rets.append(ret)
 
         if not any([i[0] for i in rets]):
             print "\t\tData management seems OK"
@@ -155,9 +187,9 @@ def main():
     parser = OptionParser(usage=usage)
 #    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
 #            default="False", help="Print verbose results")
-    parser.add_option("-c", "--only-ces", dest="onlyce", action="store_true",
+    parser.add_option("-c", "--ces", dest="onlyce", action="store_true",
             default=False, help="Check only Computing Elements")
-    parser.add_option("-s", "--only-ses", dest="onlyse", action="store_true",
+    parser.add_option("-s", "--ses", dest="onlyse", action="store_true",
             default=False, help="Check only Storage Elements")
 
     (opts, args) = parser.parse_args()
@@ -169,19 +201,18 @@ def main():
     else:
         all = True
 
-    
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "Usage %s <siteBDII>" % sys.argv[0]
+    if len(args) != 1:
+        parser.error("Error, you have to specify one (and only one) siteBDII")
 
     ret = commands.getstatusoutput("voms-proxy-info --vo")
     print_error(ret, exit=True)
 
     vo = ret[1]
 
-    bdii = sys.argv[-1]
+    bdii = args[-1]
     check_bdii(bdii)
 
-    if all or pts.onlyce:
+    if all or opts.onlyce:
        check_ces(bdii, vo)
     if all or opts.onlyse:
        check_ses(bdii, vo)
